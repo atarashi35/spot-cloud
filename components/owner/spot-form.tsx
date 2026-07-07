86ae32dd2bfd54c97e35e3b0ca5673332e7c9ff7
Ryohei Atarashi <spotcloud2026@gmail.com>
2026-07-07 17:15:59 +0900

    Lock down membership and storage rules

--- components/owner/spot-form.tsx
+++ components/owner/spot-form.tsx
@@ -461,7 +461,7 @@ export function SpotForm(props: SpotFormProps) {
         <GalleryUploader
           values={galleryImageUrls}
           onChange={setGalleryImageUrls}
-          storagePath={props.mode === "edit" ? `spots/${props.spotId}/gallery` : "spots/new/gallery"}
+          storagePath={props.mode === "edit" ? `spots/${props.spotId}/gallery` : `spots/${user.uid}/gallery`}
         />
       </div>
 
